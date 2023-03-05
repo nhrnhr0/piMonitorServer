3466e1ac03f2a0fa9eb7eca055c876930622e9d7
nhrnhr0 <nhrnhr0@gmail.com>
2023-03-05 14:14:15 +0200

    reorder asgi

--- server/asgi.py
+++ server/asgi.py
@@ -10,16 +10,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-from channels.auth import AuthMiddlewareStack
-from channels.routing import URLRouter
 from django.urls import path
-from pi import routing
-from pi.consumers import ChatConsumer
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from pi import routing
+from pi.consumers import ChatConsumer
+from channels.routing import URLRouter
 
 
 application = ProtocolTypeRouter({
